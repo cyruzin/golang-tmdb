0c692185a9b8aa33299299722dc850b122ab4d61
Cyro Dubeux <dev09@sptcode.com>
2019-01-15 10:06:08 -0300

    SessionWithLogin / Struct fix

--- authentication.go
+++ authentication.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 )
 
-// Authentication type is a struct for authentication JSON response.
-type Authentication struct {
+// RequestToken type is a struct for request token JSON response.
+type RequestToken struct {
 	Success      bool   `json:"success"`
 	ExpiresAt    string `json:"expires_at"`
 	RequestToken string `json:"request_token"`
@@ -17,15 +17,22 @@ type Session struct {
 	SessionID string `json:"session_id"`
 }
 
+// SessionWithLogin type is a struct for session with login JSON response.
+type SessionWithLogin struct {
+	Username     string `json:"username"`
+	Password     string `json:"password"`
+	RequestToken string `json:"request_token"`
+}
+
 // CreateRequestToken creates a temporary request token
 // that can be used to validate a TMDb user login.
 //
 // Query String: api_key.
 //
 // https://developers.themoviedb.org/3/authentication/create-request-token
-func (c *Client) CreateRequestToken() (*Authentication, error) {
+func (c *Client) CreateRequestToken() (*RequestToken, error) {
 	tmdbURL := fmt.Sprintf("%s%stoken/new?api_key=%s", baseURL, authenticationURL, c.APIKey)
-	a := Authentication{}
+	a := RequestToken{}
 	err := c.get(tmdbURL, &a)
 	if err != nil {
 		return nil, err
@@ -41,13 +48,38 @@ func (c *Client) CreateRequestToken() (*Authentication, error) {
 // Query String: api_key.
 //
 // https://developers.themoviedb.org/3/authentication/create-session
-func (c *Client) CreateSession(requestToken string) (*Session, error) {
+func (c *Client) CreateSession(rt string) (*Session, error) {
 	tmdbURL := fmt.Sprintf("%s%ssession/new?api_key=%s", baseURL, authenticationURL, c.APIKey)
-	rt := Authentication{RequestToken: requestToken}
+	requestToken := RequestToken{RequestToken: rt}
 	a := Session{}
-	err := c.post(tmdbURL, &rt, &a)
+	err := c.post(tmdbURL, &requestToken, &a)
 	if err != nil {
 		return nil, err
 	}
 	return &a, nil
 }
+
+// CreateSessionWithLogin creates a new session id using login.
+//
+// This method allows an application to validate a request token
+// by entering a username and password. Not all applications have
+// access to a web view so this can be used as a substitute.
+// If you decide to use this method please use HTTPS.
+//
+// Query String: api_key.
+//
+// https://developers.themoviedb.org/3/authentication/validate-request-token
+// func (c *Client) CreateSessionWithLogin(u, p, rt string) (*RequestToken, error) {
+// 	tmdbURL := fmt.Sprintf("%s%stoken/validate_with_login?api_key=%s", baseURL, authenticationURL, c.APIKey)
+// 	loginSession := SessionWithLogin{
+// 		Username:     u,
+// 		Password:     p,
+// 		RequestToken: rt,
+// 	}
+// 	a := RequestToken{}
+// 	err := c.post(tmdbURL, &loginSession, &a)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return &a, nil
+// }
