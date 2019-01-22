6e9c217ef535dbdd7532500495acaf37c98e1e73
Cyro Dubeux <xorycx@gmail.com>
2019-01-22 20:48:58 -0300

    Test typo error fixed

--- tv_test.go
+++ tv_test.go
@@ -200,14 +200,14 @@ func (suite *TMBDTestSuite) TestGetTVRecommendationsWithOptions() {
 	suite.Equal(int64(1), flash.Page)
 }
 
-func (suite *TMBDTestSuite) TestGetTVScredenedTheatrically() {
-	flash, err := suite.GetTVScredenedTheatrically(flashID)
+func (suite *TMBDTestSuite) TestGetTVScreenedTheatrically() {
+	flash, err := suite.GetTVScreenedTheatrically(flashID)
 	suite.Nil(err)
 	suite.Equal(int64(flashID), flash.ID)
 }
 
-func (suite *TMBDTestSuite) TestGetTVScredenedTheatricallyFail() {
-	_, err := suite.GetTVScredenedTheatrically(0)
+func (suite *TMBDTestSuite) TestGetTVScreenedTheatricallyFail() {
+	_, err := suite.GetTVScreenedTheatrically(0)
 	suite.Equal("The resource you requested could not be found.", err.Error())
 }
 
