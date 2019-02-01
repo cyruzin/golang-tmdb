8b5ed3188c3f1412da39797b523a7ddced532524
Cyro Dubeux <xorycx@gmail.com>
2019-01-31 21:25:22 -0300

    Review Details

--- reviews.go
+++ reviews.go
@@ -1,1 +1,30 @@
 package tmdb
+
+import "fmt"
+
+// ReviewDetails type is a struct for details JSON response.
+type ReviewDetails struct {
+	ID         string `json:"id"`
+	Author     string `json:"author"`
+	Content    string `json:"content"`
+	Iso639_1   string `json:"iso_639_1"`
+	MediaID    int64  `json:"media_id"`
+	MediaTitle string `json:"media_title"`
+	MediaType  string `json:"media_type"`
+	URL        string `json:"url"`
+}
+
+// GetReviewDetails get review details by id.
+//
+// https://developers.themoviedb.org/3/movies/get-movie-details
+func (c *Client) GetReviewDetails(id string) (*ReviewDetails, error) {
+	tmdbURL := fmt.Sprintf(
+		"%s/review/%s?api_key=%s", baseURL, id, c.APIKey,
+	)
+	t := ReviewDetails{}
+	err := c.get(tmdbURL, &t)
+	if err != nil {
+		return nil, err
+	}
+	return &t, nil
+}
--- reviews_test.go
+++ reviews_test.go
@@ -1,1 +1,15 @@
 package tmdb
+
+const reviewID = "5488c29bc3a3686f4a00004a"
+
+func (suite *TMBDTestSuite) TestGetReviewDetails() {
+	review, err := suite.GetReviewDetails(reviewID)
+	suite.Nil(err)
+	suite.NotNil(review)
+}
+
+func (suite *TMBDTestSuite) TestGetReviewDetailsFail() {
+	suite.APIKey = ""
+	_, err := suite.GetReviewDetails(reviewID)
+	suite.NotNil(err)
+}
