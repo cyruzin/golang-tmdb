b6a6d9884c85d22f0f5631e9ca686f73302a1f28
Cyro Dubeux <xorycx@gmail.com>
2019-02-01 19:50:09 -0300

    Some changes
    Some json.RawMessage fields are now structs.

--- search.go
+++ search.go
@@ -1,7 +1,6 @@
 package tmdb
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/url"
 )
@@ -71,10 +70,48 @@ type SearchMovies struct {
 
 // SearchMulti type is a struct for multi JSON response.
 type SearchMulti struct {
-	Page         int64           `json:"page"`
-	Results      json.RawMessage `json:"results"`
-	TotalResults int64           `json:"total_results"`
-	TotalPages   int64           `json:"total_pages"`
+	Page    int `json:"page"`
+	Results []struct {
+		PosterPath       string   `json:"poster_path,omitempty"`
+		Popularity       float32  `json:"popularity"`
+		ID               int64    `json:"id"`
+		Overview         string   `json:"overview,omitempty"`
+		BackdropPath     string   `json:"backdrop_path,omitempty"`
+		VoteAverage      float32  `json:"vote_average,omitempty"`
+		MediaType        string   `json:"media_type"`
+		FirstAirDate     string   `json:"first_air_date,omitempty"`
+		OriginCountry    []string `json:"origin_country,omitempty"`
+		GenreIDs         []int64  `json:"genre_ids,omitempty"`
+		OriginalLanguage string   `json:"original_language,omitempty"`
+		VoteCount        int64    `json:"vote_count,omitempty"`
+		Name             string   `json:"name,omitempty"`
+		OriginalName     string   `json:"original_name,omitempty"`
+		Adult            bool     `json:"adult,omitempty"`
+		ReleaseDate      string   `json:"release_date,omitempty"`
+		OriginalTitle    string   `json:"original_title,omitempty"`
+		Title            string   `json:"title,omitempty"`
+		Video            bool     `json:"video,omitempty"`
+		ProfilePath      string   `json:"profile_path,omitempty"`
+		KnownFor         []struct {
+			PosterPath       string  `json:"poster_path"`
+			Adult            bool    `json:"adult"`
+			Overview         string  `json:"overview"`
+			ReleaseDate      string  `json:"release_date"`
+			OriginalTitle    string  `json:"original_title"`
+			GenreIDs         []int64 `json:"genre_ids"`
+			ID               int64   `json:"id"`
+			MediaType        string  `json:"media_type"`
+			OriginalLanguage string  `json:"original_language"`
+			Title            string  `json:"title"`
+			BackdropPath     string  `json:"backdrop_path"`
+			Popularity       float32 `json:"popularity"`
+			VoteCount        int64   `json:"vote_count"`
+			Video            bool    `json:"video"`
+			VoteAverage      float32 `json:"vote_average"`
+		} `json:"known_for,omitempty"`
+	} `json:"results"`
+	TotalResults int64 `json:"total_results"`
+	TotalPages   int64 `json:"total_pages"`
 }
 
 // SearchPeople type is a struct for people JSON response.
