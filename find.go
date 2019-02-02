18be44042fa30d1f50f768d1aa1ff960d0f9937b
Cyro Dubeux <xorycx@gmail.com>
2019-02-01 22:24:48 -0300

    Find

--- find.go
+++ find.go
@@ -1,1 +1,104 @@
 package tmdb
+
+import "fmt"
+
+// FindByID type is a struct for find JSON response.
+type FindByID struct {
+	MovieResults []struct {
+		Adult            bool    `json:"adult"`
+		BackdropPath     string  `json:"backdrop_path"`
+		GenreIDs         []int64 `json:"genre_ids"`
+		ID               int64   `json:"id"`
+		OriginalLanguage string  `json:"original_language"`
+		OriginalTitle    string  `json:"original_title"`
+		Overview         string  `json:"overview"`
+		PosterPath       string  `json:"poster_path"`
+		ReleaseDate      string  `json:"release_date"`
+		Title            string  `json:"title"`
+		Video            bool    `json:"video"`
+		VoteAverage      float32 `json:"vote_average"`
+		VoteCount        int64   `json:"vote_count"`
+		Popularity       float32 `json:"popularity"`
+	} `json:"movie_results,omitempty"`
+	PersonResults []struct {
+		Adult    bool   `json:"adult"`
+		Gender   int    `json:"gender"`
+		Name     string `json:"name"`
+		ID       int64  `json:"id"`
+		KnownFor []struct {
+			Adult            bool    `json:"adult"`
+			BackdropPath     string  `json:"backdrop_path"`
+			GenreIDs         []int64 `json:"genre_ids"`
+			ID               int64   `json:"id"`
+			OriginalLanguage string  `json:"original_language"`
+			OriginalTitle    string  `json:"original_title"`
+			Overview         string  `json:"overview"`
+			PosterPath       string  `json:"poster_path"`
+			ReleaseDate      string  `json:"release_date"`
+			Title            string  `json:"title"`
+			Video            bool    `json:"video"`
+			VoteAverage      float32 `json:"vote_average"`
+			VoteCount        int64   `json:"vote_count"`
+			Popularity       float32 `json:"popularity"`
+			MediaType        string  `json:"media_type"`
+		} `json:"known_for"`
+		KnownForDepartment string  `json:"known_for_department"`
+		ProfilePath        string  `json:"profile_path"`
+		Popularity         float32 `json:"popularity"`
+	} `json:"person_results,omitempty"`
+	TvResults []struct {
+		OriginalName     string   `json:"original_name"`
+		ID               int64    `json:"id"`
+		Name             string   `json:"name"`
+		VoteCount        int64    `json:"vote_count"`
+		VoteAverage      float32  `json:"vote_average"`
+		FirstAirDate     string   `json:"first_air_date"`
+		PosterPath       string   `json:"poster_path"`
+		GenreIDs         []int64  `json:"genre_ids"`
+		OriginalLanguage string   `json:"original_language"`
+		BackdropPath     string   `json:"backdrop_path"`
+		Overview         string   `json:"overview"`
+		OriginCountry    []string `json:"origin_country"`
+		Popularity       float32  `json:"popularity"`
+	} `json:"tv_results,omitempty"`
+	TvEpisodeResults []struct {
+		AirDate        string  `json:"air_date"`
+		EpisodeNumber  int     `json:"episode_number"`
+		ID             int64   `json:"id"`
+		Name           string  `json:"name"`
+		Overview       string  `json:"overview"`
+		ProductionCode string  `json:"production_code"`
+		SeasonNumber   int     `json:"season_number"`
+		ShowID         int64   `json:"show_id"`
+		StillPath      string  `json:"still_path"`
+		VoteAverage    float32 `json:"vote_average"`
+		VoteCount      int64   `json:"vote_count"`
+	} `json:"tv_episode_results,omitempty"`
+	TvSeasonResults []struct {
+		AirDate      string `json:"air_date"`
+		Name         string `json:"name"`
+		ID           int64  `json:"id"`
+		SeasonNumber int    `json:"season_number"`
+		ShowID       int64  `json:"show_id"`
+	} `json:"tv_season_results,omitempty"`
+}
+
+// GetFindByID the find method makes it easy to search for objects in our
+// database by an external id. For example, an IMDB ID.
+//
+// This method will search all objects (movies, TV shows and people)
+// and return the results in a single response.
+//
+// https://developers.themoviedb.org/3/find/find-by-id
+func (c *Client) GetFindByID(id string, o map[string]string) (*FindByID, error) {
+	options := c.fmtOptions(o)
+	tmdbURL := fmt.Sprintf(
+		"%s/find/%s?api_key=%s%s", baseURL, id, c.APIKey, options,
+	)
+	k := FindByID{}
+	err := c.get(tmdbURL, &k)
+	if err != nil {
+		return nil, err
+	}
+	return &k, nil
+}
--- find_test.go
+++ find_test.go
@@ -1,1 +1,21 @@
 package tmdb
+
+const theWalkingDeadID = "tt1520211"
+
+func (suite *TMBDTestSuite) TestGetFindByID() {
+	options := make(map[string]string)
+	options["external_source"] = "imdb_id"
+	options["language"] = "en-US"
+	genres, err := suite.GetFindByID(theWalkingDeadID, options)
+	suite.Nil(err)
+	suite.NotNil(genres)
+}
+
+func (suite *TMBDTestSuite) TestGetFindByIDFail() {
+	suite.APIKey = ""
+	options := make(map[string]string)
+	options["external_source"] = "imdb_id"
+	options["language"] = "en-US"
+	_, err := suite.GetFindByID(theWalkingDeadID, options)
+	suite.NotNil(err)
+}
