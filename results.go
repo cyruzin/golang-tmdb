09f1574e450e5d66851ed8b68a876cb98c3ebf72
Cyro Dubeux <xorycx@gmail.com>
2021-10-02 13:34:35 -0300

    embedded tv results types implemented

--- results.go
+++ results.go
@@ -1,5 +1,7 @@
 package tmdb
 
+// Movie Embedded Result Types
+
 type MovieReleaseDatesResults struct {
 	Results []struct {
 		Iso3166_1    string `json:"iso_3166_1"`
@@ -132,5 +134,136 @@ type MoviePopularResults struct {
 		VoteCount        int64   `json:"vote_count"`
 		Video            bool    `json:"video"`
 		VoteAverage      float32 `json:"vote_average"`
+	} `json:"results"`
+}
+
+// TV Embedded Result Types
+
+type TVAlternativeTitlesResults struct {
+	Results []struct {
+		Iso3166_1 string `json:"iso_3166_1"`
+		Title     string `json:"title"`
+		Type      string `json:"type"`
+	} `json:"results"`
+}
+
+type TVContentRatingsResults struct {
+	Results []struct {
+		Iso3166_1 string `json:"iso_3166_1"`
+		Rating    string `json:"rating"`
+	} `json:"results"`
+}
+
+type TVEpisodeGroupsResults struct {
+	Results []struct {
+		Description  string `json:"description"`
+		EpisodeCount int    `json:"episode_count"`
+		GroupCount   int    `json:"group_count"`
+		ID           string `json:"id"`
+		Name         string `json:"name"`
+		Network      struct {
+			ID            int64  `json:"id"`
+			LogoPath      string `json:"logo_path"`
+			Name          string `json:"name"`
+			OriginCountry string `json:"origin_country"`
+		} `json:"network"`
+		Type int `json:"type"`
+	} `json:"results"`
+}
+
+type TVKeywordsResults struct {
+	Results []struct {
+		ID   int64  `json:"id"`
+		Name string `json:"name"`
+	} `json:"results"`
+}
+
+type TVRecommendationsResults struct {
+	Results []struct {
+		PosterPath       string   `json:"poster_path"`
+		Popularity       float32  `json:"popularity"`
+		ID               int64    `json:"id"`
+		BackdropPath     string   `json:"backdrop_path"`
+		VoteAverage      float32  `json:"vote_average"`
+		Overview         string   `json:"overview"`
+		FirstAirDate     string   `json:"first_air_date"`
+		OriginCountry    []string `json:"origin_country"`
+		GenreIDs         []int64  `json:"genre_ids"`
+		OriginalLanguage string   `json:"original_language"`
+		VoteCount        int64    `json:"vote_count"`
+		Name             string   `json:"name"`
+		OriginalName     string   `json:"original_name"`
+	} `json:"results"`
+}
+
+type TVReviewsResults struct {
+	Results []struct {
+		ID      string `json:"id"`
+		Author  string `json:"author"`
+		Content string `json:"content"`
+		URL     string `json:"url"`
+	} `json:"results"`
+}
+
+type TVScreenedTheatricallyResults struct {
+	Results []struct {
+		ID            int64 `json:"id"`
+		EpisodeNumber int   `json:"episode_number"`
+		SeasonNumber  int   `json:"season_number"`
+	} `json:"results"`
+}
+
+type TVWatchProvidersResults struct {
+	Results map[string]struct {
+		Link     string `json:"link"`
+		Flatrate []struct {
+			DisplayPriority int64  `json:"display_priority"`
+			LogoPath        string `json:"logo_path"`
+			ProviderID      int64  `json:"provider_id"`
+			ProviderName    string `json:"provider_name"`
+		} `json:"flatrate,omitempty"`
+		Rent []struct {
+			DisplayPriority int64  `json:"display_priority"`
+			LogoPath        string `json:"logo_path"`
+			ProviderID      int64  `json:"provider_id"`
+			ProviderName    string `json:"provider_name"`
+		} `json:"rent,omitempty"`
+		Buy []struct {
+			DisplayPriority int64  `json:"display_priority"`
+			LogoPath        string `json:"logo_path"`
+			ProviderID      int64  `json:"provider_id"`
+			ProviderName    string `json:"provider_name"`
+		} `json:"buy,omitempty"`
+	} `json:"results"`
+}
+
+type TVVideosResults struct {
+	Results []struct {
+		ID        string `json:"id"`
+		Iso639_1  string `json:"iso_639_1"`
+		Iso3166_1 string `json:"iso_3166_1"`
+		Key       string `json:"key"`
+		Name      string `json:"name"`
+		Site      string `json:"site"`
+		Size      int    `json:"size"`
+		Type      string `json:"type"`
+	} `json:"results"`
+}
+
+type TVAiringTodayResults struct {
+	Results []struct {
+		OriginalName     string   `json:"original_name"`
+		GenreIDs         []int64  `json:"genre_ids"`
+		Name             string   `json:"name"`
+		Popularity       float32  `json:"popularity"`
+		OriginCountry    []string `json:"origin_country"`
+		VoteCount        int64    `json:"vote_count"`
+		FirstAirDate     string   `json:"first_air_date"`
+		BackdropPath     string   `json:"backdrop_path"`
+		OriginalLanguage string   `json:"original_language"`
+		ID               int64    `json:"id"`
+		VoteAverage      float32  `json:"vote_average"`
+		Overview         string   `json:"overview"`
+		PosterPath       string   `json:"poster_path"`
 	} `json:"results"`
 }
