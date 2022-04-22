3ae5b698fa8c9743f1b957a3a53169bb209c556d
Cyro Dubeux <xorycx@gmail.com>
2022-04-22 14:30:17 -0300

    added append_to_response example

---
+++ examples/appendToResponse/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tmdb "github.com/cyruzin/golang-tmdb"
+)
+
+func main() {
+	tmdbClient, err := tmdb.Init(os.Getenv("APIKey"))
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// With options
+	options := make(map[string]string)
+	options["append_to_response"] = "watch/providers"
+
+	movie, err := tmdbClient.GetMovieDetails(299536, options)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, v := range movie.MovieWatchProvidersAppend.WatchProviders.Results {
+		fmt.Println(v.Flatrate)
+	}
+}
